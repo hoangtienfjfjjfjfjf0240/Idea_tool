743fb14c5fb7149105e9377991a8047bd9de8707
hoangtienfjfjjfjfjf0240 <hoangtienfjfjjfjfjf0240@gmail.com>
2026-04-19 22:16:14 +0700

    add dev origin config and workflow svg

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['127.0.0.1'],
   // Allow larger file uploads (videos up to 50MB)
   experimental: {
     serverActions: {
